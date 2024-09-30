50c5f8ea1b2163f9ce742dcbc93c16ba412a9dae
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-09-30 14:50:20 +0600

    faq

--- js/main.js
+++ js/main.js
@@ -8,3 +8,42 @@ $(window).scroll(function () {
   }
 });
 // nav scroll end
+
+// after scrolling indicate active nav-link start
+$(document).on("scroll", function () {
+  const sections = $("section");
+  const navLinks = $(".nav-link");
+
+  let currentSection = "";
+
+  sections.each(function () {
+    const sectionTop = $(this).offset().top;
+    const sectionHeight = $(this).outerHeight();
+    if ($(window).scrollTop() >= sectionTop - sectionHeight / 3) {
+      currentSection = $(this).attr("id");
+    }
+  });
+
+  navLinks.each(function () {
+    $(this).removeClass("active");
+    if ($(this).attr("href").includes(currentSection)) {
+      $(this).addClass("active");
+    }
+  });
+});
+// after scrolling indicate active nav-link end
+
+// counter up start
+$(document).ready(function () {
+  $(".counter").counterUp({
+    delay: 10,
+    time: 2000,
+  });
+});
+// counter up end
+
+// copy right year start
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById("year").innerHTML = year;
+// copy right year end
